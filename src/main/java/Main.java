bda016c681b4f5bb8493b878201c37b516fe1074
KingsMMA <63374258+KingsMMA@users.noreply.github.com>
2021-12-11 05:59:09 +1000

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.apache.commons.codec.binary.Base32;
+
+import java.security.SecureRandom;
+
+public class Main {
+
+    public static void main(String[] args) {
+		SecureRandom random = new SecureRandom();
+		byte[] bytes = new byte[20];
+		random.nextBytes(bytes);
+		Base32 base32 = new Base32();
+		System.out.println(base32.encodeToString(bytes));
+    }
+
+}
